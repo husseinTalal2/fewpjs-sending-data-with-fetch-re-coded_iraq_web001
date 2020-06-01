c2936f1da2fcf558e0c336e5d10b40eeef4924c7
Hussein Talal <hossientalal2@gmail.com>
2020-06-01 10:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+
+
+// fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object)
+//   })
+//   .catch(function(error) {
+//   alert("Bad things! Ragnarők!");
+//   console.log(error.message);})
+const userName = document.getElementById('name').value;
+const Email = document.getElementById('email').value;
+
+
+function submitData(userName, Email){
+  let formData = {
+    dogName: userName,
+    dogBreed: Email
+  };
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  return fetch("http://localhost:3000/users",configObj)
+}
